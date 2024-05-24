677a0879ace2d4057f912f16c80bef45d655f896
Sergio Wicramachine <wicramachine@wicra-2.local>
2024-05-24 15:10:09 +0200

    Teste Ping

--- teste_ping.php
+++ teste_ping.php
@@ -14,3 +14,20 @@
         echo "<p>L'adresse IP $ip_address est inactive.</p>";
     }
     ?>
+
+
+
+<?php
+    // Adresse IP du PC à vérifier
+    $ip_address = "192.168.1.100";
+
+    // Exécute la commande ping et récupère le résultat
+    exec("ping -c 1 $ip_address", $output, $result);
+
+    // Vérifie si la commande ping a réussi (0) ou non (autre chose que 0)
+    if ($result == 0) {
+        echo "<p>L'adresse IP $ip_address est active.</p>";
+    } else {
+        echo "<p>L'adresse IP $ip_address est inactive.</p>";
+    }
+    ?>
