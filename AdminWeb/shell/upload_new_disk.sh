b1e24c20c1814589a6257997062f7d5a0ee353f2
wsergio <wsergio@stagiaire3.PERRIN>
2024-06-18 15:45:32 +0200

    upgrade upload_new_disk and ScriptIPXE.sh for faill of security

--- AdminWeb/shell/upload_new_disk.sh
+++ AdminWeb/shell/upload_new_disk.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Deplacer le nouveau disk vers repertroir images
 sudo mv /var/www/html/AdminWeb/upload_new_disk_tmp/* /images/
+#Pas tres secure d'ajouter la possibilité de chown sur le site
+sudo chown  modele:modele /image/*.raw
